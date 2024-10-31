2f0ad0ee29ffff6619fc91cd1193ab7e251fa764
KryptonianSon <kryptonianson@users.noreply.github.com>
2024-10-31 10:46:15 -0400

    Update pagemon.rb

--- pagemon.rb
+++ pagemon.rb
@@ -4,7 +4,6 @@ class Pagemon < Formula
   url "https://github.com/binarynoir/pagemon/archive/v1.0.1.tar.gz"
   sha256 "818b23c20b9baff9372b7afbdaefad46708b18a5f5f20f3d43d7129841c30568"
   
-  depends_on "libxml2"
   depends_on "jq" => :optional
   depends_on "md5sha1sum"
 
