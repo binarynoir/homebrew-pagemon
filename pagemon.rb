8c5f207a7d0fe052f9d27e23868409e8263fe14c
KryptonianSon <kryptonianson@users.noreply.github.com>
2024-10-31 10:43:08 -0400

    Update pagemon.rb

--- pagemon.rb
+++ pagemon.rb
@@ -1,8 +1,8 @@
 class Pagemon < Formula
   desc "A tool to monitor web pages for changes"
   homepage "https://github.com/binarynoir/pagemon"
-  url "https://github.com/binarynoir/pagemon/archive/v1.0.0.tar.gz"
-  sha256 "50b388c15282f964966cd7da96728b00e78c504c74a725d37dd420b8aee976fd"
+  url "https://github.com/binarynoir/pagemon/archive/v1.0.1.tar.gz"
+  sha256 "818b23c20b9baff9372b7afbdaefad46708b18a5f5f20f3d43d7129841c30568"
   
   depends_on "libxml2"
   depends_on "jq" => :optional
